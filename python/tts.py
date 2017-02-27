bb52d44d13ae7c291867a0c4d823374f006206c7
Maren Hall-Wieckert <mhallwie@rgnt2-87-143-dhcp.int.colorado.edu>
2017-02-26 17:49:38 -0700

    final working with video

--- python/tts.py
+++ python/tts.py
@@ -7,9 +7,10 @@ import threading
 from OSC import OSCClient, OSCMessage
 from OSC import OSCServer
 import collections
+import statistics
 
 
-server = OSCServer( ("localhost", 12000) )
+server = OSCServer( ("localhost", 12001) )
 server.timeout = 0
 run = True
 
@@ -37,7 +38,7 @@ thread_flag = True
 
 
 
-d = collections.deque([],maxlen = 10)
+d = collections.deque([],maxlen = 1)
 
 
 
@@ -51,57 +52,62 @@ def respond_voice(value):
 	global not_count
 	global thread_flag
 	print "\nResponse Generating"
-
+        print d[0]
 	if d[0] != current_output:
 		not_count = not_count + 1
-		if not_count > 50:
+		if not_count > 1:
 			thread_flag = False
 
 			if d[0] == 1.0 :
-				print "LEFT"
-				speech('LEFT')
+				print "Straight"
+				speech('Straight')
 				current_output = 1.0
 			if d[0] == 2.0 :
-				print "Right"
-				speech('Right')
+				print "Turn"
+				speech('Turn')
 				current_output = 2.0
+                        '''
 			if d[0] == 3.0 :
 				print "Obstacle"
 				speech('Obstacle')
 				current_output = 3.0
-			if d[0] == 4.0 :
-				print "Nothing"
-				speech('Nothing')
+                        
+                        if d[0] == 4.0 :
+				print "Straight"
+				speech('Straight')
 				current_output = 4.0
-			if d[0] == 5.0 :
-				print "Nothing"
-				speech('Nothing')
-				current_output = 5.0
-			if d[0] == 6.0 :
-				print "default"
-				#speech('Awesome')
-				current_output = 6.0
+			'''
+			
 	thread_flag = True
 	return
 
 
-
-
-
 def response(path,tags, args, source):
-	print args[0]
+	#print args[0]
 	global count
 	global not_count
 	global current_output
 	global thread_flag	
 
-	global d 
-	d.append(args[0])
-	print current_output
+	global d
+        stat_array = []
+	for i in range(10):
+            stat_array.append(args[i])
+        mode = statistics.mode(stat_array)
+        '''
+        print mode
+        '''
+	#print args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7]
+	
+	d.append(mode)
+	#print current_output
 	#print d[0]
-	if d[0] != current_output:
-		print "thread_spawned"
-		respond_voice(d[0])
+	if mode != current_output:
+		if mode == 1.0:
+                    print "Straight"
+                elif mode == 2.0:
+                    print "Turn"
+		respond_voice(mode)
 
 		
 	
@@ -120,7 +126,7 @@ def speech(input_text):
 	
 	tts = gTTS(text=input_text,lang = 'en')
 	tts.save("good.mp3")
-	os.system("cmdmp3 good.mp3")
+	os.system("mpg321 good.mp3")
 
 
 def main():
